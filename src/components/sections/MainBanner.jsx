481884c9d4888c64c0c07a27dbb1bcd78417dbeb
cyhammad <hammad727272@gmail.com>
2025-04-03 11:44:28 +0500

    new changes pushed

--- src/components/sections/MainBanner.jsx
+++ src/components/sections/MainBanner.jsx
@@ -1,32 +1,39 @@
-"use client"
-import { companyName } from "@/libs/data";
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import { ImagesSlider } from "./ImagesSlider";
 import CallAndWhatsappButton from "../buttons/CallAndWhatsappButton";
-import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
-const MainBanner = ({ company = companyName }) => {
-  const pathname = usePathname();
-
-  // Dynamically set company name based on route
-  const dynamicCompany =
-    pathname === "/companies/water-heater" ? "Water Heater" : company;
+function MainBanner({ company = "Appliance Service Center UAE" }) {
+  const images = ["/static/banner-1.jpeg", "/static/banner-2.jpeg", "/static/banner-3.jpeg"];
 
   return (
-    <div className="flex items-center justify-center w-full bg-[url(/static/main-banner.jpg)] bg-cover bg-fixed bg-no-repeat bg-center min-h-[35rem] h-full">
-      <div className="bg-black/75 text-white w-full h-full min-h-[35rem] flex items-center justify-center">
-        <div className="flex flex-col uppercase text-center px-5 py-10">
-          <h2 className="mb-3">
-            Reliable <span className="text-primary">&</span> Affordable
-          </h2>
-          <h1 className="text-4xl uppercase">We Offer Repair Service across UAE. </h1>
-          <h1 className="text-4xl font-bold">{dynamicCompany} repairs</h1>
-          <CallAndWhatsappButton
-            company={dynamicCompany}
-            className="py-5 justify-center"
-          />
-        </div>
-      </div>
-    </div>
+    <ImagesSlider
+      className={cn("h-[40rem]", company === "Siemens" ? "mt-0" : "mt-0")}
+      images={images}
+    >
+      <motion.div
+        initial={{ opacity: 0, y: -80 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="z-50 flex flex-col justify-center items-center"
+      >
+        <motion.p className="font-bold text-3xl md:text-5xl px-5 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+        Authorized {company.toUpperCase()} Service Center - Fast, reliable repairs for a hassle-free home.
+        </motion.p>
+        <motion.p className="max-w-7xl mb-5 px-5 md:text-lg text-white text-center">
+          At our Service Center, our expert technicians are dedicated to restoring your appliances to optimal performance,
+          ensuring unmatched comfort and convenience. Whether addressing minor
+          issues or managing significant breakdowns, we offer unparalleled
+          service to keep your household running seamlessly. Trust us for
+          premium appliance repair that guarantees efficiency and
+          reliability.
+        </motion.p>
+        <CallAndWhatsappButton company={company} banner />
+      </motion.div>
+    </ImagesSlider>
   );
-};
+}
 
 export default MainBanner;
